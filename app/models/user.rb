0643e6f6aaff3525f7a97194b89f79a23ba73297
Garik Kosai <garik@Gariks-MBP.hsd1.wa.comcast.net>
2016-04-27 13:56:30 -0700

    User profile description update working with localhost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :events, dependent: :destroy
-	has_many :messages, dependent: :destroy
+	# has_many :messages, dependent: :destroy
 
 	has_many :attendees, dependent: :destroy
     has_many :events_attending, through: :attendees, source: :event
