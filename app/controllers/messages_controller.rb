77968c2e489cc112b63419309251b0211269d96b
Jessica Wilson <jessicalostinspace@gmail.com>
2016-06-09 22:48:14 -0700

    rendered json output for table view data in message controller instead of array data

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,16 +9,14 @@ class MessagesController < ApplicationController
   end
 
   def show
-    user = User.find_by facebook_id: params[:facebook_id]
+    user = User.find_by facebook_id: params[:id]
     # user = User.where('facebook_id = ?', params[:facebook_id])
-
-    # user_ids = Message.where(receiver_id: user.id).select(:sender_id).distinct
 
     msgArr = Array.new
 
     # grabs names and picture urls for table view of messages
     # puts them in msgArray
-    Message.where(receiver_id: user.id).select(:sender_id).distinct.each { |mess| msgArr<<mess.sender.first_name + " " + mess.sender.last_name + " " + mess.sender.picture_url }
+ Message.where(receiver_id: user.id).select(:sender_id).distinct.each { |mess| msgArr<<{first_name: mess.sender.first_name.to_s,  last_name:  mess.sender.last_name.to_s, picture_url: mess.sender.picture_url.to_s }}
 
     render json: msgArr
 
