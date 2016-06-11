622f2250c54850ced7c4937c335304db6574257a
Jessica Wilson <jessicalostinspace@gmail.com>
2016-06-11 15:57:03 -0700

    implemented a migration file to add event image_url to events table to pull in on events table view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-
   skip_before_filter :verify_authenticity_token
 
   def index
---
+++ db/migrate/20160611225003_add_column_to_events.rb
@@ -0,0 +1,5 @@
+class AddColumnToEvents < ActiveRecord::Migration
+  def change
+  	add_column :events, :image_url, :string
+  end
+end
